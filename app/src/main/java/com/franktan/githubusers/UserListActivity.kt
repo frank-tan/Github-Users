5e4c72c38c19835495f16e1795effb3270b8c961
Frank Tan <frank.tan@rea-group.com>
2017-06-19 22:06:09 +1000

    create initial project

---
+++ app/src/main/java/com/franktan/githubusers/UserListActivity.kt
@@ -0,0 +1,12 @@
+package com.franktan.githubusers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_list_layout)
+    }
+}
